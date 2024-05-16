aadc80d0ed95b0926573e2df7af5a5a7e5ef3265
phuochungus <21520252@gm.uit.edu.vn>
2024-05-16 12:15:56 +0700

    chore: Update entrypoint.sh script to run train.sh in the background and wait for background processes to complete

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-./train.sh ./exps/simplecil.json
+# Run the long-running train.sh script in the background
+./train.sh ./exps/simplecil.json &
+
+# Run the entrypoint.sh script
+date 
+
+# Wait for all background processes to complete
+wait
