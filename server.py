457c08044cab3fbd14e3d7a22ad93cb415165c62
phuochungus <21520252@gm.uit.edu.vn>
2024-05-16 17:45:07 +0700

    chore: Update server.py to include /train endpoint

--- server.py
+++ server.py
@@ -1,12 +1,33 @@
 from flask import Flask
 import subprocess
+import logging
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+logging.basicConfig(level=logging.DEBUG)
+
+sentry_sdk.init(
+    dsn="https://0dbd8387b1c5f375af0fe420ea5f15f2@o4507264673710080.ingest.us.sentry.io/4507264677969920",
+    integrations=[
+        LoggingIntegration(
+            level=logging.DEBUG,  # Capture info and above as breadcrumbs
+            event_level=logging.DEBUG,  # Send records as events
+        ),
+    ],
+)
 
 app = Flask(__name__)
 
-@app.get('/')
-def index():
-    subprocess.call(['./entrypoint.sh'])
-    return 'Bash script triggered successfully!'
 
-if __name__ == '__main__':
-    app.run()
+@app.get("/")
+def index():
+    return "Hello, World!"
+
+@app.get("/train")
+def index():
+    subprocess.call(["./entrypoint.sh"])
+    return "Bash script triggered successfully!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
