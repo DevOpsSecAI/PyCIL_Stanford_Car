19fcbf1b9baba9575c0cc3345390cd8ce01395f4
phuochungus <21520252@gm.uit.edu.vn>
2024-05-16 18:30:45 +0700

    chore: Add /train endpoint to server.py

--- server.py
+++ server.py
@@ -1,27 +1,7 @@
 from flask import Flask
 import subprocess
-import logging
-import sentry_sdk
-from sentry_sdk.integrations.logging import LoggingIntegration
-
-logging.basicConfig(level=logging.DEBUG)
-
-sentry_sdk.init(
-    dsn="https://0dbd8387b1c5f375af0fe420ea5f15f2@o4507264673710080.ingest.us.sentry.io/4507264677969920",
-    integrations=[
-        LoggingIntegration(
-            level=logging.DEBUG,  # Capture info and above as breadcrumbs
-            event_level=logging.DEBUG,  # Send records as events
-        ),
-    ],
-)
 
 app = Flask(__name__)
-
-
-@app.get("/")
-def index():
-    return "Hello, World!"
 
 @app.get("/train")
 def train():
