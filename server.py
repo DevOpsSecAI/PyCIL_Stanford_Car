65887f8102a15161af278598baae52b499230a96
phuochungus <21520252@gm.uit.edu.vn>
2024-06-09 12:01:30 +0700

    chore: Update server.py to run Flask app on specified host and port

--- server.py
+++ server.py
@@ -11,5 +11,4 @@ def train():
 
 
 if __name__ == "__main__":
-    # run subprocess.call(["./train.sh", "./exps/simplecil.json"]) in background after starting the server 3s
-    subprocess.call(["./train.sh", "./exps/simplecil.json"])
+    app.run(host="0.0.0.0", port=7860)
