e0098dbd0c9c7df05bdf90a1126953c7d433d4fc
Kawaiibuta <163984214+Kawaiibuta@users.noreply.github.com>
2024-06-29 10:33:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-from utils.data_manager import DataManager
-a =  DataManager(
-        'general_dataset',
-        True,
-        2003,
-        20,
-        20,
-        path = 'resources',
-    )
+
